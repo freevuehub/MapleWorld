0e8e2f5a1c9677baab8045b68bae4ad33cde6eff
freevuehub <hackish113@gmail.com>
2024-01-19 02:42:18 +0900

    테스트 코드 작성

--- FX.test.lua
+++ FX.test.lua
@@ -1,12 +1,53 @@
 dofile("./FX.lua")
 
-Pipe(
-	{1, 2, 3, 4},
-	Map(
-		function(value)
-			return value + 1
+local function test(name, input)
+	print("-- " .. name .. " Test")
+
+	return function(check)
+		if input ~= check then
+			error("")
 		end
-	),
-	Sum,
-	print
-)
+
+		print("Pass!")
+		print("")
+	end
+end
+
+test(
+	"Head",
+	Pipe(
+		{1, 2, 3, 4},
+		Head
+	)
+)(1)
+test(
+	"Map",
+	Pipe(
+		{1, 2, 3, 4},
+		Map(
+			function(value)
+				return value + 1
+			end
+		),
+		Sum
+	)
+)(14)
+test(
+	"Reduce",
+	Pipe(
+		{ "Hello", "World" },
+		Reduce(
+			function(prev, cur)
+				return prev .. cur
+			end,
+			""
+		)
+	)
+)("HelloWorld")
+test(
+	"Sum",
+	Pipe(
+		{ 1, 2, 3, 4, 5 },
+		Sum
+	)
+)(15)
